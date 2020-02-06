47089a62b42f49d4a419ed8051e197d9121da95a
Banty <bnty.kmr@gmail.com>
2020-02-06 15:28:39 +0530

    project setup on git completed

---
+++ bookservicebackend/src/main/java/dev/banty/bookservicebackend/App.java
@@ -0,0 +1,13 @@
+package dev.banty.bookservicebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
